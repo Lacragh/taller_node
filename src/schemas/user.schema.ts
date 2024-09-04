eb344e03745de9960123e225b2cb667179b06d6b
Victor Manuel Garzon Meneses <101030206+Lacragh@users.noreply.github.com>
2024-09-03 22:56:21 -0500

    Update user.schema.ts

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,11 +1,12 @@
-import { object, string, TypeOf } from 'zod';
+import { object, string } from 'zod';
 
-const userSchema = object ({
-
-    name: string({ required_error: "Name is required" }),
-    email: string({ required_error: "Email is required" }).email("Not a valid email address"),
-    password: string({ required_error: "Password is required" }).min(8, "Password must be at least 8 characters long")
-
+// Esquema de validación Zod para los datos de un usuario
+const userSchema = object({
+    name: string({ required_error: "Name is required" }),  // Nombre del usuario, obligatorio
+    email: string({ required_error: "Email is required" })  // Correo electrónico, obligatorio y debe ser una dirección de correo válida
+        .email("Not a valid email address"),  // Valida que el correo electrónico tenga un formato correcto
+    password: string({ required_error: "Password is required" })  // Contraseña, obligatoria
+        .min(8, "Password must be at least 8 characters long")  // Valida que la contraseña tenga al menos 8 caracteres
 });
 
 export default userSchema;
