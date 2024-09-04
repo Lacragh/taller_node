7939d5cb7825b41830055a99ab87f449808dc825
Victor Manuel Garzon Meneses <101030206+Lacragh@users.noreply.github.com>
2024-09-03 22:47:47 -0500

    Update validateSchema.ts

--- src/middlewares/validateSchema.ts
+++ src/middlewares/validateSchema.ts
@@ -1,13 +1,14 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import { AnyZodObject } from "zod";
 
+// Middleware para validar el cuerpo de la solicitud usando un esquema Zod
 const validateSchema = (schema: AnyZodObject) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
-            await schema.parseAsync(req.body);
-            next();
+            await schema.parseAsync(req.body);  // Intenta validar el cuerpo de la solicitud contra el esquema Zod
+            next();  // Si la validación es exitosa, llama al siguiente middleware o controlador
         } catch (error) {
-            res.status(400).json(error);
+            res.status(400).json(error);  // Devuelve un error 400 Bad Request si la validación falla
         }
     }
 }
