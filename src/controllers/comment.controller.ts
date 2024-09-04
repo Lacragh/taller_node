b342703ac3d7268c08853be88ea432086096bd48
Victor Manuel Garzon Meneses <101030206+Lacragh@users.noreply.github.com>
2024-09-03 22:44:13 -0500

    Update comment.controller.ts

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -4,79 +4,56 @@ import CommentService from "../services/comment.service";
 
 class CommentController {
 
+    // Crea un nuevo comentario
     public async create(req: Request, res: Response) {
-
         try {
-
             const { userId, content, parentId } = req.body;
             const comment: CommentDocument = await CommentService.create({ userId, content, parentId });
             return res.status(201).json(comment);
-
         } catch (error) {
-
             return res.status(500).json(error);
-
         }
+    }
 
-    };
-
+    // Obtiene un comentario por su ID
     public async get(req: Request, res: Response) {
-
         try {
-
             const comment: CommentDocument | null = await CommentService.findById(req.params.id);
             return res.json(comment);
-            
-
         } catch (error) {
-
             return res.status(500).json(error);
-
         }
+    }
 
-    };
-
+    // Obtiene todos los comentarios
     public async getAll(req: Request, res: Response) {
-
         try {
-
             const comments: CommentDocument[] = await CommentService.findAll();
             return res.json(comments);
-
         } catch (error) {
-
             return res.status(500).json(error);
-
         }
+    }
 
-    };
-
+    // Obtiene comentarios por el ID del comentario principal (parentId)
     public async getByParentId(req: Request, res: Response) {
-
         try {
-            
             const comments: CommentDocument[] = await CommentService.findByParentId(req.params.parentId);
             return res.json(comments);
-
         } catch (error) {
-
-           return res.status(500).json(error);
-
+            return res.status(500).json(error);
         }
+    }
 
-    };
-
+    // Actualiza un comentario por su ID
     public async update(req: Request, res: Response) {
-
         try {
-
             const comment: CommentDocument | null = await CommentService.findById(req.params.id);
             
             if (!comment) {
                 return res.status(404).json({ message: `Comment with id: ${req.params.id} not found` });
             }
 
-    
             const { id } = req.body.loggedUser; 
             if (comment.userId.toString() !== id) {
                 return res.status(403).json({ message: "You do not have permission to modify this comment" });
@@ -84,19 +61,14 @@ class CommentController {
 
             const updatedComment: CommentDocument | null = await CommentService.update(req.params.id, req.body as CommentInput);
             return res.json(updatedComment);
-
         } catch (error) {
-
-           return res.status(500).json(error);
-
+            return res.status(500).json(error);
         }
+    }
 
-    };
-
+    // Elimina un comentario por su ID
     public async delete(req: Request, res: Response) {
-
         try {
-
             const comment: CommentDocument | null = await CommentService.findById(req.params.id);
             
             if (!comment) {
@@ -108,18 +80,12 @@ class CommentController {
                 return res.status(403).json({ message: "You do not have permission to delete this comment" });
             }
 
-            
             await CommentService.delete(req.params.id);
             return res.json({ message: "Comment deleted successfully" });
-
         } catch (error) {
-
             return res.status(500).json(error);
-
         }
-        
-    };
-
-};
+    }
+}
 
 export default new CommentController();
