c54852fc088012d98164d50653aea5bb529d6260
Victor Manuel Garzon Meneses <101030206+Lacragh@users.noreply.github.com>
2024-09-03 22:47:19 -0500

    Update auth.ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,25 +1,28 @@
 import { Request, Response, NextFunction } from "express";
-import jwt,{TokenExpiredError} from "jsonwebtoken";
+import jwt, { TokenExpiredError } from "jsonwebtoken";
 
-const auth = async(req:Request, res:Response, next:NextFunction)=>{
+// Middleware para autenticar solicitudes usando JWT
+const auth = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        let token: string | undefined = req.headers.authorization;
-        if(!token)
-            res.status(401).json({message:"Not Authorized"});
-        else{
-            token = token.replace("Bearer ", "");
-            const decoded: any = jwt.verify(token, process.env.JWT_SECRET || "secret");
-            req.body.loggedUser = decoded
-            req.params.role = decoded.role;
-            next();
+        let token: string | undefined = req.headers.authorization;  // Obtiene el token de los encabezados de autorización
+        
+        if (!token) {
+            return res.status(401).json({ message: "Not Authorized" });  // Devuelve 401 si no hay token
+        } else {
+            token = token.replace("Bearer ", "");  // Elimina el prefijo "Bearer " del token
+            const decoded: any = jwt.verify(token, process.env.JWT_SECRET || "secret");  // Verifica y decodifica el token usando la clave secreta
+            req.body.loggedUser = decoded;  // Agrega el usuario decodificado al cuerpo de la solicitud
+            req.params.role = decoded.role;  // Agrega el rol decodificado a los parámetros de la solicitud (esto podría ser innecesario o debería estar en req.body)
+            next();  // Llama al siguiente middleware o controlador
         }
 
-    }catch(error){
-        if(error instanceof TokenExpiredError)
-            res.status(401).json({message:"Token Expired", error})
-        else
-            res.status(401).json({message:"Token Invalid", error})
-
+    } catch (error) {
+        if (error instanceof TokenExpiredError) {
+            return res.status(401).json({ message: "Token Expired", error });  // Devuelve 401 si el token ha expirado
+        } else {
+            return res.status(401).json({ message: "Token Invalid", error });  // Devuelve 401 si el token es inválido
+        }
     }
-}
+};
+
 export default auth;
