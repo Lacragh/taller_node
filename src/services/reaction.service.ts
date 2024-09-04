b82794af9fc8812839509fd0f8072d44d5831fdf
Victor Manuel Garzon Meneses <101030206+Lacragh@users.noreply.github.com>
2024-09-03 22:57:53 -0500

    Update reaction.service.ts

--- src/services/reaction.service.ts
+++ src/services/reaction.service.ts
@@ -2,29 +2,32 @@ import ReactionModel, { ReactionDocument, ReactionInput } from "../models/reacti
 
 class ReactionService {
     
+    // Crea una nueva reacción en la base de datos
     public async create(reactionInput: ReactionInput): Promise<ReactionDocument> {
         try {
             const reaction = await ReactionModel.create(reactionInput);
             return reaction;
         } catch (error) {
-            throw error;
+            throw error;  // Propaga cualquier error que ocurra
         }
     }
 
+    // Encuentra todas las reacciones asociadas a un comentario específico
     public async findByCommentId(commentId: string): Promise<ReactionDocument[]> {
         try {
             return ReactionModel.find({ commentId }).populate('userId', 'name email').exec();
         } catch (error) {
-            throw error;
+            throw error;  // Propaga cualquier error que ocurra
         }
     }
 
+    // Elimina una reacción específica basada en el usuario y el comentario
     public async deleteByUserAndComment(userId: string, commentId: string): Promise<ReactionDocument | null> {
         try {
             const reaction: ReactionDocument | null = await ReactionModel.findOneAndDelete({ userId, commentId });
-            return reaction
+            return reaction;
         } catch (error) {
-            throw error;
+            throw error;  // Propaga cualquier error que ocurra
         }
     }
 }
