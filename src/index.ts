47b678f105282690eb9719261701f908261963cf
Manuel Herrera <98495072+Herre1@users.noreply.github.com>
2024-11-19 19:51:45 -0500

    APOLLO SERVER

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from 'dotenv';
-// Agregar logica de Apollo server si es necesario
-
-import { router } from './routes/posts';
-import { router as user } from './routes/user';
-import { router as comment } from './routes/comment';
-import { router as reaction } from './routes/reaction';
-import { db } from './config/db'
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import bodyParser from "body-parser";
+import 'graphql-import-node';
+import typeDefs from './graphql/schema.graphql';
+import { resolvers } from "./graphql/resolvers";
+import { router } from "./routes/posts";
+import { router as user } from "./routes/user";
+import { router as comment } from "./routes/comment";
+import { router as reaction } from "./routes/reaction";
+import { db } from "./config/db";
 
 dotenv.config();
 console.log(process.env.DBPASS);
@@ -14,27 +19,44 @@ console.log(process.env.DBPASS);
 const app: Express = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response)=>{
+async function startApolloServer() {
+  // Configurar Apollo Server
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-    res.send('Hello World');
+  await server.start();
 
-});
+  // Usar Apollo como middleware de Express
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server)
+  );
 
-app.use('/api/v1/posts', router);
-app.use('/api/v1/user', user);
-app.use('/api/v1/comment', comment);
-app.use('/api/v1/reaction', reaction);
+  // Rutas REST
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
 
-db.then( () => {
+  app.use("/api/v1/posts", router);
+  app.use("/api/v1/user", user);
+  app.use("/api/v1/comment", comment);
+  app.use("/api/v1/reaction", reaction);
 
-    app.listen(port, ()=> {
-
-        console.log(`Server is running on port ${port}`);
-    
+  // Conexión a la base de datos y servidor
+  db.then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+      console.log(`GraphQL endpoint: http://localhost:${port}/graphql`);
     });
+  });
+}
 
-});
+startApolloServer();
